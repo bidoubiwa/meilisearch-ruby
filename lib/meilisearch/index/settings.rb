fa3a95a63896b83bdbe304186b91207d31dee467
Clementine Urquizar <clementine@meilisearch.com>
2020-03-03 17:04:24 +0100

    Upd first part of settings part

--- lib/meilisearch/index/settings.rb
+++ lib/meilisearch/index/settings.rb
@@ -3,25 +3,36 @@
 module MeiliSearch
   class Index < HTTPRequest
     module Settings
+
+      # General routes
       def settings
         http_get "/indexes/#{@uid}/settings"
       end
       alias get_settings settings
 
-      def add_or_replace_settings(options)
-        http_post "/indexes/#{@uid}/settings", options
+      def update_settings(settings)
+        http_post "/indexes/#{@uid}/settings", settings
       end
-      alias add_settings add_or_replace_settings
-      alias replace_settings add_or_replace_settings
 
-      def reset_all_settings
-        body = {
-          rankingOrder: nil,
-          distinctField: nil,
-          rankingRules: nil
-        }
-        http_post "/indexes/#{@uid}/settings", body
+      def reset_settings
+        http_delete "/indexes/#{@uid}/settings"
       end
+
+      # Sub-routes ranking rules
+      def ranking_rules
+        http_get "/indexes/#{@uid}/settings/ranking-rules"
+      end
+      alias get_ranking_rules ranking_rules
+
+      def update_ranking_rules(ranking_rules)
+        http_post "/indexes/#{@uid}/settings/ranking-rules", ranking_rules
+      end
+
+      def reset_ranking_rules
+        http_delete "/indexes/#{@uid}/ranking-rules"
+      end
+
+
     end
   end
 end
--- spec/meilisearch/index/settings_spec.rb
+++ spec/meilisearch/index/settings_spec.rb
@@ -2,74 +2,89 @@
 
 RSpec.describe MeiliSearch::Index::Settings do
   before(:all) do
-    documents = [
-      { objectId: 123,  title: 'Pride and Prejudice' },
-      { objectId: 456,  title: 'Le Petit Prince' },
-      { objectId: 1,    title: 'Alice In Wonderland' },
-      { objectId: 1344, title: 'The Hobbit' },
-      { objectId: 4,    title: 'Harry Potter and the Half-Blood Prince' },
-      { objectId: 42,   title: 'The Hitchhiker\'s Guide to the Galaxy' }
-    ]
     client = MeiliSearch::Client.new($URL, $API_KEY)
-    @index = client.create_index('Index name')
-    @index.add_documents(documents)
-    sleep(0.1)
+    clear_all_indexes(client)
+    @index = client.create_index('indexUID')
   end
 
   after(:all) do
     @index.delete
   end
 
-  it 'gets index settings' do
-    response = @index.settings
-    expect(response).to be_a(Hash)
-    expect(response).not_to be_empty
-    expect(response).to have_key('rankingOrder')
-    expect(response['rankingOrder']).to eq(nil)
-    expect(response).to have_key('distinctField')
-    expect(response['distinctField']).to eq(nil)
-    expect(response).to have_key('rankingRules')
-    expect(response['rankingRules']).to eq(nil)
-  end
-
-  it 'add ranking rules' do
-    response = @index.add_settings(rankingRules: { objectId: 'asc' })
-    expect(response).to have_key('updateId')
-    sleep(0.1)
-    expect(@index.settings['rankingRules']['objectId']).to eq('asc')
-    skip 'waiting for next version' do
-      search = @index.search('prince')
-      expect(search['hits'][0]['objectId'] < search['hits'][1]['objectId'])
+  context 'On global settings routes' do
+    it 'gets default values of setting' do
+      response = @index.settings
+      expect(response).to be_a(Hash)
+      expect(response).not_to be_empty
+      expect(response.keys).to contain_exactly(
+        'rankingRules',
+        'distinctAttribute',
+        'searchableAttributes',
+        'displayedAttributes',
+        'stopWords',
+        'synonyms',
+        'acceptNewFields'
+      )
+      expect(response['rankingRules']).to eq([
+        'typo',
+        'words',
+        'proximity',
+        'attribute',
+        'wordsPosition',
+        'exactness'
+      ])
+      expect(response['distinctAttribute']).to be_nil
+      expect(response['searchableAttributes']).to be_nil
+      expect(response['displayedAttributes']).to be_nil
+      expect(response['stopWords']).to eq([])
+      expect(response['synonyms']).to eq({})
+      expect(response['acceptNewFields']).to be_truthy
     end
-  end
 
-  it 'replaces ranking rules' do
-    response = @index.replace_settings(rankingRules: { title: 'asc' })
-    expect(response).to have_key('updateId')
-    sleep(0.1)
-    settings = @index.settings
-    expect(settings['rankingRules']['title']).to eq('asc')
-    expect(settings['rankingRules']).not_to have_key('objectId')
-  end
-
-  it 'resets all settings' do
-    response = @index.reset_all_settings
-    expect(response).to have_key('updateId')
-    sleep(0.1)
-    skip 'waiting for next version' do
+    it 'updates multiples settings at the same time' do
+      response = @index.update_settings(
+        rankingRules: ['asc(title)', 'typo'],
+        distinctAttribute: 'title'
+      )
+      expect(response).to have_key('updateId')
+      sleep(0.1)
       settings = @index.settings
-      expect(settings).to have_key('rankingOrder')
-      expect(settings['rankingOrder']).to eq(nil)
-      expect(settings).to have_key('distinctField')
-      expect(settings['distinctField']).to eq(nil)
-      expect(settings).to have_key('rankingRules')
-      expect(settings['rankingRules']).to eq(nil)
+      expect(settings['rankingRules']).to eq(['asc(title)', 'typo'])
+      expect(settings['distinctAttribute']).to eq('title')
+      expect(settings['stopWords']).to be_empty
+    end
+
+    it 'updates one setting without reset the others' do
+      response = @index.update_settings(stopWords: ['the'])
+      expect(response).to have_key('updateId')
+      sleep(0.1)
+      settings = @index.settings
+      expect(settings['rankingRules']).to eq(['asc(title)', 'typo'])
+      expect(settings['distinctAttribute']).to eq('title')
+      expect(settings['stopWords']).to eq(['the'])
+      expect(settings['synonyms']).to be_empty
+    end
+
+    it 'resets all settings' do
+      response = @index.reset_settings
+      expect(response).to have_key('updateId')
+      sleep(0.1)
+      settings = @index.settings
+      expect(settings['rankingRules']).to eq([
+        'typo',
+        'words',
+        'proximity',
+        'attribute',
+        'wordsPosition',
+        'exactness'
+      ])
+      expect(settings['distinctAttribute']).to be_nil
+      expect(settings['stopWords']).to be_empty
+      expect(settings['synonyms']).to be_empty
     end
   end
 
   it 'works with method aliases' do
     expect(@index.method(:settings) == @index.method(:get_settings)).to be_truthy
-    expect(@index.method(:add_or_replace_settings) == @index.method(:add_settings)).to be_truthy
-    expect(@index.method(:add_or_replace_settings) == @index.method(:replace_settings)).to be_truthy
   end
 end
