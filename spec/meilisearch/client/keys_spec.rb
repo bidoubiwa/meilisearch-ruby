afebd13094686098d174b611eac52a8c5c98150f
Clementine Urquizar <clementine@meilisearch.com>
2020-03-10 16:03:56 +0100

    Add permissions tests

--- spec/meilisearch/client/keys_spec.rb
+++ spec/meilisearch/client/keys_spec.rb
@@ -3,6 +3,8 @@
 RSpec.describe MeiliSearch::Client::Keys do
   before(:all) do
     @client = MeiliSearch::Client.new($URL, $API_KEY)
+    @uid = 'uid'
+    index = @client.create_index(@uid)
   end
 
   it 'gets the list of keys' do
@@ -14,16 +16,35 @@ RSpec.describe MeiliSearch::Client::Keys do
     expect(response['public']).to be_a(String)
   end
 
-  it 'fails to get keys if public key used' do
+  it 'fails to get settings if public key used' do
     public_key = @client.keys['public']
     new_client = MeiliSearch::Client.new($URL, public_key)
-    expect { new_client.keys }.to raise_meilisearch_http_error_with(403)
+    expect { new_client.index(@uid).settings }.to raise_meilisearch_http_error_with(403)
   end
 
   it 'fails to get keys if private key used' do
-    public_key = @client.keys['private']
-    new_client = MeiliSearch::Client.new($URL, public_key)
+    private_key = @client.keys['private']
+    new_client = MeiliSearch::Client.new($URL, private_key)
     expect { new_client.keys }.to raise_meilisearch_http_error_with(403)
+  end
+
+  it 'fails to search if no key used' do
+    new_client = MeiliSearch::Client.new($URL)
+    expect { new_client.index(@uid).settings }.to raise_meilisearch_http_error_with(403)
+  end
+
+  it 'succeeds to search when using public key' do
+    public_key = @client.keys['public']
+    new_client = MeiliSearch::Client.new($URL, public_key)
+    response = new_client.index(@uid).search('test')
+    expect(response).to have_key('hits')
+  end
+
+  it 'succeeds to get settings when using private key' do
+    public_key = @client.keys['public']
+    new_client = MeiliSearch::Client.new($URL, public_key)
+    response = new_client.index(@uid).settings
+    expect(response).to have_key('rankingRules')
   end
 
   it 'works with method aliases' do
